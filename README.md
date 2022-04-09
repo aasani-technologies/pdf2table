73d76939515f314248e8ffd3a7f7a5b204660f64
kudchikarsk <kudchikarsk@gmail.com>
2022-04-10 00:38:25 +0530

    update line detection logic

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - [https://www.digitalocean.com/community/tutorials/typescript-new-project](https://www.digitalocean.com/community/tutorials/typescript-new-project)
 - [https://github.com/modesty/pdf2json#page-object-reference](https://github.com/modesty/pdf2json#page-object-reference)
 - [https://github.com/Automattic/node-canvas](https://github.com/Automattic/node-canvas)
+- [https://stackoverflow.com/questions/42494394/pdf2json-page-unit-what-is-it](https://stackoverflow.com/questions/42494394/pdf2json-page-unit-what-is-it)
--- src/table-parser.ts
+++ src/table-parser.ts
@@ -3,41 +3,58 @@ import PDFParser from "pdf2json";
 import { Logger } from "log4js";
 import { promisify } from 'util';
 import { createCanvas, CanvasRenderingContext2D, Canvas } from 'canvas';
-import { PdfData, Text } from './interfaces/pdf-data';
+import { PdfData, Text, Vline, Page, Hline } from './interfaces/pdf-data';
 
 const wrtieFilePromise = promisify(fs.writeFile);
 
+export interface PageLayout {
+    width:number;
+    height:number;
+    resolution:number;
+}
+
 export class TableParser {
 
-    pdfData:PdfData;
-    scale = 100;
+    pdfData: PdfData;
+    pageLayout: PageLayout = {
+        width: 2480,
+        height: 3508,
+        resolution: 72
+    }
 
-    constructor(private pdfParser:PDFParser, private logger:Logger) {
+    constructor(private pdfParser: PDFParser, private logger: Logger) {
         this.pdfData = {} as PdfData;
     }
 
-    loadPdf(filePath:string): Promise<void> {
-        const result = new Promise<void>((resolve,reject)=>{
+    transform(data:{x:number, y:number}, page:Page) {
+        return({
+            x: data.x * ((this.pageLayout.width) / page.Width),
+            y: data.y * ((this.pageLayout.height) / page.Height)
+        });
+    }
+
+    loadPdf(filePath: string): Promise<void> {
+        const result = new Promise<void>((resolve, reject) => {
             this.pdfParser.on("pdfParser_dataError", errData => reject(errData));
-            this.pdfParser.on("pdfParser_dataReady", pdfData => {  
+            this.pdfParser.on("pdfParser_dataReady", pdfData => {
                 this.pdfData = pdfData;
                 this.logger.info(`${filePath} loaded!`);
                 resolve();
             });
 
             this.pdfParser.loadPDF(filePath)
-            .then(()=> this.logger.info(`${filePath} loadPdf completed!`))
-            .catch(err=> {
-                this.logger.error(err);
-                reject(err);
-            });
-        })       
-        
+                .then(() => this.logger.info(`${filePath} loadPdf completed!`))
+                .catch(err => {
+                    this.logger.error(err);
+                    reject(err);
+                });
+        })
+
         return result;
     }
 
-    async saveJson(filePath:string): Promise<void> {
-      
+    async saveJson(filePath: string): Promise<void> {
+
         try {
             await wrtieFilePromise(filePath, JSON.stringify(this.pdfData));
             return this.logger.info(`${filePath} saved!`);
@@ -46,37 +63,62 @@ export class TableParser {
         }
     }
 
-    getPageCanvas() {
-        const page = this.pdfData.Pages[0];
-        const width = this.scale * page.Width;
-        const height = this.scale * page.Height;
-        const canvas = createCanvas(width, height);
+    getPageCanvas() {        
+        const canvas = createCanvas(this.pageLayout.width, this.pageLayout.height);
         const context = canvas.getContext('2d');
         context.fillStyle = '#ffffff'
-        context.fillRect(0, 0, width, height);
+        context.fillRect(0, 0, this.pageLayout.width, this.pageLayout.height);
         return canvas;
-    } 
+    }
 
-    drawText(canvas:Canvas, text:Text) {
+    drawText(canvas: Canvas, page:Page, text: Text) {
         const context = canvas.getContext('2d');
         const r = text.R[0];
         const fontSize = r.TS[1];
         context.fillStyle = '#000000';
-        context.font = `${fontSize}pt Arial`;
+        context.font = `${fontSize * 2}pt sans-serif`;
         context.textAlign = text.A as CanvasTextAlign;
         const rawText = r.T.replace(/%20/g, '').trim();
-        context.fillText(rawText, text.x * this.scale, text.y * this.scale);
+        const point = this.transform({x: text.x, y:text.y}, page);
+        const depth = this.transform({x:text.w, y:text.sw}, page);
+        const offsetY = (this.pageLayout.width/this.pageLayout.height) * 50;
+        const offsetX = (this.pageLayout.width/this.pageLayout.height) * 20;
+        context.fillText(rawText, point.x + offsetX, point.y + offsetY, depth.x);
     }
 
-    drawPage(pageIndex:number){      
-        const canvas = this.getPageCanvas(); 
+    drawPage(pageIndex: number) {
+        const canvas = this.getPageCanvas();
         const page = this.pdfData.Pages[pageIndex];
-        page.Texts.forEach(text => this.drawText(canvas, text))
-        return canvas;    
+        page.Texts.forEach(text => this.drawText(canvas, page, text))
+        page.VLines.forEach(line=>this.drawVLine(canvas, page, line));
+        page.HLines.forEach(line=>this.drawHLine(canvas, page, line));
+        return canvas;
+    }
+
+    drawVLine(canvas: Canvas, page:Page, line: Vline) {
+        const context = canvas.getContext('2d');
+        context.strokeStyle = 'rgba(0,0,0,0.5)';
+        context.beginPath();
+        const point = this.transform({x:line.x, y:line.y}, page);
+        const depth = this.transform({x:line.w, y:line.l}, page);
+        context.lineTo(point.x, point.y);
+        context.lineTo(point.x, point.y + depth.y);
+        context.stroke();
+    }
+
+    drawHLine(canvas: Canvas, page:Page, line: Hline) {
+        const context = canvas.getContext('2d');
+        context.strokeStyle = 'rgba(0,0,0,0.5)';
+        context.beginPath();
+        const point = this.transform({x:line.x, y:line.y}, page);
+        const depth = this.transform({x:line.w, y:line.l}, page);
+        context.lineTo(point.x, point.y);
+        context.lineTo(point.x + depth.y, point.y);
+        context.stroke();
     }
 
 
-    async saveImage(pageIndex:number, filePath:string): Promise<void> {
+    async saveImage(pageIndex: number, filePath: string): Promise<void> {
         const canvas = this.drawPage(pageIndex);
         const buffer = canvas.toBuffer('image/png');
         try {
@@ -84,6 +126,6 @@ export class TableParser {
             return this.logger.info(`${filePath} saved!`);
         } catch (err) {
             return this.logger.error(err);
-        }    
+        }
     }
 }
