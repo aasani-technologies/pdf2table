97f4f56a33a20b32af9d48d218e05e1fc3fca3bf
kudchikarsk <kudchikarsk@gmail.com>
2022-05-04 21:18:23 +0530

    add declaration files and sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,24 @@ const gulp = require('gulp');
 const minify = require('gulp-minify');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
+const merge = require('merge2');
+const sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('default', () => {
-  return tsProject
-    .src()
-    .pipe(tsProject())
-    .js.pipe(
-      minify({
-        ext: {
-          min: '.js',
-        },
-        noSource: true,
-      })
-    )
-    .pipe(gulp.dest('build'));
+  const tsResult = tsProject.src().pipe(sourcemaps.init()).pipe(tsProject());
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest('build')),
+    tsResult.js
+      .pipe(
+        minify({
+          ext: {
+            min: '.js',
+          },
+          noSource: true,
+        })
+      )
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('build'))
+  ]);
 });
